90b872b15e7a86b1d8e1714b09820fcd6b66c6cc
Jirapat Khowinthawong <jk24jirapat@gmail.com>
2023-08-27 00:22:47 +0700

    Update: login controller

--- src/main/java/cs211/project/controllers/LoginController.java
+++ src/main/java/cs211/project/controllers/LoginController.java
@@ -72,15 +72,10 @@ public class LoginController {
 
     @FXML
     private void handleRegisterButtonClick(ActionEvent event) throws Exception {
-<<<<<<< HEAD
-        FXRouter.goTo("register");
-=======
         try {
             FXRouter.goTo("register");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
->>>>>>> feature/login
     }
-
 }
