12c0f574ece03cfb3f27eaab3ebb6f162299987f
Yada Rattanavaraha <yada.rat@ku.th>
2023-08-27 00:48:07 +0700

    Update: footer

--- src/main/java/cs211/project/controllers/EventDetailController.java
+++ src/main/java/cs211/project/controllers/EventDetailController.java
@@ -8,13 +8,22 @@ import java.io.IOException;
 public class EventDetailController {
     @FXML
     private AnchorPane navbar;
-
+    @FXML
+    private AnchorPane footer;
     @FXML
     private void initialize() {
+        //Navbar
         FXMLLoader navbarComponentLoader = new FXMLLoader(getClass().getResource("/cs211/project/views/navbar.fxml"));
+        //Footer
+        FXMLLoader footerComponentLoader = new FXMLLoader(getClass().getResource("/cs211/project/views/footer.fxml"));
         try {
+            //Navbar
             AnchorPane navbarComponent = navbarComponentLoader.load();
             navbar.getChildren().add(navbarComponent);
+
+            //Footer
+            AnchorPane footerComponent = footerComponentLoader.load();
+            footer.getChildren().add(footerComponent);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
