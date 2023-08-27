950cc16ce8d9688d4b3e3e64712f43cf0c27060a
Yada Rattanavaraha <yada.rat@ku.th>
2023-08-27 19:04:44 +0700

    Add: AllEventController & fxml

---
+++ src/main/java/cs211/project/controllers/AllEventController.java
@@ -0,0 +1,33 @@
+package cs211.project.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+public class AllEventController {
+    @FXML
+    private AnchorPane navbar;
+    @FXML
+    private AnchorPane footer;
+
+    @FXML
+    private void initialize() {
+        //Navbar
+        FXMLLoader navbarComponentLoader = new FXMLLoader(getClass().getResource("/cs211/project/views/navbar.fxml"));
+        //Footer
+        FXMLLoader footerComponentLoader = new FXMLLoader(getClass().getResource("/cs211/project/views/footer.fxml"));
+        try {
+            //Navbar
+            AnchorPane navbarComponent = navbarComponentLoader.load();
+            navbar.getChildren().add(navbarComponent);
+
+            //Footer
+            AnchorPane footerComponent = footerComponentLoader.load();
+            footer.getChildren().add(footerComponent);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
