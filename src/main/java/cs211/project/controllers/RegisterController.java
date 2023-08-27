d41a8e949ed47aee7ce35de85743cf0112e8b798
Jirapat Khowinthawong <jk24jirapat@gmail.com>
2023-08-27 11:44:37 +0700

    Update: register view && register controller

--- src/main/java/cs211/project/controllers/RegisterController.java
+++ src/main/java/cs211/project/controllers/RegisterController.java
@@ -2,15 +2,10 @@ package cs211.project.controllers;
 
 import cs211.project.services.FXRouter;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 
 import java.io.IOException;
