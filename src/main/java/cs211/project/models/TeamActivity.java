e57024862e7468d2fedc7d4f6b3a25f48fc631a8
prokittikun <pro.kittikun.2546@gmail.com>
2023-10-06 02:53:45 +0700

    Refactor:implement class inheritance from Activity

---
+++ src/main/java/cs211/project/models/TeamActivity.java
@@ -0,0 +1,39 @@
+package cs211.project.models;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class TeamActivity extends Activity{
+
+        private UUID teamId;
+        public TeamActivity(String id, String teamId, String name, String detail, String startDate, String startTime, String endDate, String endTime) {
+            super(id, name, detail, startDate, startTime, endDate, endTime);
+            this.teamId = UUID.fromString(teamId);
+        }
+
+        public TeamActivity(HashMap<String, String> activity) {
+            super(activity);
+            this.teamId = UUID.fromString(activity.get("teamId"));
+        }
+
+        public String getTeamId() {
+            return teamId.toString();
+        }
+
+        public void setTeamId(String eventId) {
+            this.teamId = UUID.fromString(eventId);
+        }
+
+        public HashMap<String, String> toHashMap() {
+            HashMap<String, String> activity = new HashMap<>();
+            activity.put("id", this.getId());
+            activity.put("teamId", this.getTeamId());
+            activity.put("name", this.getName());
+            activity.put("detail", this.getDetail());
+            activity.put("startDate", this.getStartDate());
+            activity.put("startTime", this.getStartTime());
+            activity.put("endDate", this.getEndDate());
+            activity.put("endTime", this.getEndTime());
+            return activity;
+        }
+}
