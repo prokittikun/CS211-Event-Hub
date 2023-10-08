6b3b3a74f7ab97277bfbc36844998edb478b7952
prokittikun <pro.kittikun.2546@gmail.com>
2023-10-09 06:50:26 +0700

    Add: save message on activity

--- src/main/java/cs211/project/models/Chat.java
+++ src/main/java/cs211/project/models/Chat.java
@@ -6,24 +6,28 @@ import java.util.UUID;
 public class Chat {
     private UUID id;
     private UUID userId;
+
+    private UUID activityId;
     private String message;
 
     private UUID teamId;
     private String timestamp;
 
-    public Chat(String senderUserId, String message, String teamId, String timestamp) {
+    public Chat(String senderUserId, String message, String teamId, String activityId, String timestamp) {
         this.id = UUID.randomUUID();
         this.userId = UUID.fromString(senderUserId);
         this.message = message;
         this.teamId = UUID.fromString(teamId);
+        this.activityId = UUID.fromString(activityId);
         this.timestamp = timestamp;
     }
 
-    public Chat(HashMap<String, String> chat){
+    public Chat(HashMap<String, String> chat) {
         this.id = UUID.fromString(chat.get("id"));
         this.userId = UUID.fromString(chat.get("userId"));
         this.message = chat.get("message");
         this.teamId = UUID.fromString(chat.get("teamId"));
+        this.activityId = UUID.fromString(chat.get("activityId"));
         this.timestamp = chat.get("timestamp");
     }
 
@@ -39,6 +43,10 @@ public class Chat {
         this.timestamp = timestamp;
     }
 
+    public void setActivityId(String activityId) {
+        this.activityId = UUID.fromString(activityId);
+    }
+
     public String getSenderUserId() {
         return userId.toString();
     }
@@ -51,12 +59,23 @@ public class Chat {
         return timestamp;
     }
 
+    public String getId() {
+        return id.toString();
+    }
+    public String getTeamId() {
+        return teamId.toString();
+    }
+    public String getActivityId() {
+        return activityId.toString();
+    }
+
     public HashMap<String, String> toHashMap() {
         HashMap<String, String> chat = new HashMap<>();
         chat.put("id", String.valueOf(this.id));
         chat.put("userId", String.valueOf(this.userId));
         chat.put("message", this.message);
         chat.put("teamId", String.valueOf(this.teamId));
+        chat.put("activityId", String.valueOf(this.activityId));
         chat.put("timestamp", this.timestamp);
 
         return chat;
