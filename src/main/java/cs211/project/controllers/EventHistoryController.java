8c88c1d692bb4f3cae165f96fbe976f6779ef025
Jirapat Khowinthawong <jk24jirapat@gmail.com>
2023-10-11 17:11:48 +0700

    Update: loadEventsIntoTable

--- src/main/java/cs211/project/controllers/EventHistoryController.java
+++ src/main/java/cs211/project/controllers/EventHistoryController.java
@@ -16,22 +16,18 @@ import javafx.scene.control.Label;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.TableCell;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.Date;
 
 public class EventHistoryController {
-    @FXML
-    private TableColumn<?, ?> dateColumn;
-
-    @FXML
-    private TableColumn<?, ?> eventColumn;
-
-    @FXML
-    private TableView<?> eventHistoryTable;
-
     @FXML
     private AnchorPane footer;
 
@@ -39,14 +35,19 @@ public class EventHistoryController {
     private AnchorPane navbar;
 
     @FXML
-    private TableColumn<?, ?> orderColumn;
+    private TableView<Event> eventHistoryTable;
 
     @FXML
-    private TableColumn<?, ?> statusColumn;
-
+    private TableColumn<Event, String> joinTimeColumn;
     @FXML
-    private TableColumn<?, ?> toolColumn;
-    
+    private TableColumn<Event, ?> eventColumn;
+    @FXML
+    private TableColumn<Event, Integer> orderColumn;
+    @FXML
+    private TableColumn<Event, String> statusColumn;
+    @FXML
+    private TableColumn<Event, Void> toolColumn;
+
     private UUID userId;
     private HashMap<String, Object> data;
     private Datasource<UserCollection> userListFileDatasource;
@@ -55,10 +56,9 @@ public class EventHistoryController {
 
     @FXML
     private void initialize() {
-
         userListFileDatasource = new UserListFileDatasource("data", "user.csv");
         eventListFileDatasource = new EventListFileDatasource("data/event", "event.csv");
-        joinEventCollectionDatasource = new JoinEventListFileDatasource("data/event", "jointEvent.csv");
+        joinEventCollectionDatasource = new JoinEventListFileDatasource("data/event", "joinEvent.csv");
 
         data = FXRouter.getData();
         userId = UUID.fromString((String) data.get("userId"));
@@ -81,6 +81,35 @@ public class EventHistoryController {
         EventCollection eventCollection = eventListFileDatasource.readData();
         JoinEventCollection joinEventCollection = joinEventCollectionDatasource.readData();
 
+        loadEventsIntoTable();
+    }
+
+    private void loadEventsIntoTable() {
+        List<String> userEventIds = joinEventCollectionDatasource.readData().getEventIdsByUserId(userId);
+        List<Event> events = eventListFileDatasource.readData().getEvents();
+        //List<JoinEvent> joinEvents = joinEventCollectionDatasource.readData().getJoinEvents();
+
+        List<Event> userEvents = events.stream()
+                .filter(event -> userEventIds.contains(event.getId()))
+                .collect(Collectors.toList());
+
+        /*List<String> joinTimes = joinEvents.stream()
+                .map(JoinEvent::getJoinTime)
+                .collect(Collectors.toList());
+        List<JoinEvent> userJoinEvents = joinEvents.stream()
+                .filter(joinEvent -> userEventIds.contains(joinEvent.getEventId()))
+                .collect(Collectors.toList());*/
+
+        orderColumn.setCellValueFactory(column -> {
+            int rowIndex = eventHistoryTable.getItems().indexOf(column.getValue()) + 1;
+            return new SimpleObjectProperty<>(rowIndex);
+        });
+
+        eventColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        //joinTimeColumn.setCellValueFactory(new PropertyValueFactory<>("joinTime"));
+        statusColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getStatus()));
+
+        eventHistoryTable.setItems(FXCollections.observableArrayList(userEvents));
     }
 
 }
